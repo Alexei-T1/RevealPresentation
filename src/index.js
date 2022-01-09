a8e107edde3ae7280b3e3267bc6487e4859036b8
Alexei-T1 <turencko.alexei@yandex.ru>
2022-01-09 18:21:47 +0400

    creat buld

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import 'reveal.js/dist/reset.css'
-import 'reveal.js/dist/reveal.css'
-import 'reveal.js/dist/theme/sky.css'
-import Reveal from 'reveal.js';
-Reveal.initialize();
-
-import './styles/styles.css'
